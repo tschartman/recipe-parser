2c9a761154c75b3addc1d8b709b7b8f3f9869ce2
tim <tschartman@yahoo.com>
2020-04-14 18:07:44 -0500

    init

---
+++ api.py
@@ -0,0 +1,54 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+from flask import Flask, request, jsonify
+import json
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+def extractIngredients(soup):
+    found = False
+    try:
+        tags = soup.find(text=re.compile('Ingredients')).parent
+        lists = []
+        while not found:
+            if(tags.findAll('li')):
+                found = True
+                for tag in tags.findAll('li'):
+                    lists.append(tag.text.strip())
+            else:
+                tags = tags.parent
+        return lists
+    except AttributeError:
+        return ['Ingredients could not be found']
+
+def extractDirections(soup):
+    found = False
+    try:
+        tags = soup.find(text=re.compile('Directions')).parent
+        lists = []
+        while not found:
+            if(tags.findAll('li')):
+                found = True
+                for tag in tags.findAll('li'):
+                    lists.append(tag.text.strip())
+            else:
+                tags = tags.parent
+        return lists
+    except AttributeError:
+        return ['Directions could not be found']
+
+@app.route('/recipe', methods=['POST'])
+def home():
+    data = request.get_json()
+    response = requests.get(data['url'])
+    soup = BeautifulSoup(response.text, "html.parser")
+    recipe =  {
+        'ingeredients': extractIngredients(soup),
+        'directions': extractDirections(soup)
+    }
+    return jsonify(recipe)
+
+app.run()
+
